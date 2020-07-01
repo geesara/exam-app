e2b26994d318e4b15616b9a204b9000b3bcde91a
Sahan Geesara <sgeesara@gmail.com>
2020-07-01 19:11:42 +0530

    create useListAUDState hook

---
+++ src/hooks/useListAUDState.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export default (initialValue = []) => {
+  const [list, setList] = useState(initialValue);
+
+  const addItem = (newItem) => {
+    setList([...list, { id: uuidv4, ...newItem }]);
+  };
+  const updateItem = (id, updatedItem) => {
+    setList(
+      list.map((item) =>
+        item.id === id
+          ? {
+              id: item.id,
+              ...updatedItem,
+            }
+          : item
+      )
+    );
+  };
+  const deleteItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  return [list, addItem, updateItem, deleteItem];
+};
